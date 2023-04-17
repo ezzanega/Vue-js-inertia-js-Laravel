192d1bbbf648492ffd54ff0d251805a917a2b0f3
Abdoul Zakari <60135885+abdoul-nft@users.noreply.github.com>
2023-04-18 01:36:26 +0200

    form setup

---
+++ resources/js/Components/Organisms/CreateClientForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="pt-3 flex flex-col py-10 h-auto justify-between">
+    <div class="w-full pt-3 flex flex-col space-y-2">
+      <DefaultInput
+        :required="true"
+        name="Nom"
+        label="Nom"
+      />
+
+      <DefaultInput
+        :required="true"
+        name="Nom"
+        label="Nom"
+      />
+
+      <DefaultInput
+        :required="true"
+        name="Nom"
+        label="Nom"
+      />
+
+      <DefaultInput
+        :required="true"
+        name="Nom"
+        label="Nom"
+      />
+
+      <DefaultInput
+        :required="true"
+        name="Nom"
+        label="Nom"
+      />
+    </div>
+    <DefaultButton buttontext="Valider" />
+  </div>
+</template>
+<script setup>
+  import DefaultInput from '@/Components/Atoms/DefaultInput.vue';
+  import DefaultButton from '@/Components/Atoms/DefaultButton.vue';
+</script>  
--- resources/js/Pages/6dem/Dashboard.vue
+++ resources/js/Pages/6dem/Dashboard.vue
@@ -5,11 +5,16 @@
       <Drawer
         :is-open="isDrawerOpen"
         :speed="500"
-        max-width="900px"
+        :max-width="innerWidth"
         @close="closeDrawer"
       >
         <div>
-          <DrawerContent />
+          <DrawerContent
+            title="Create client"
+            @closeDrawer="closeDrawer()"
+          >
+            <CreateClientForm />
+          </DrawerContent>
         </div>
       </Drawer>
       <button @click="toggleDrawer">
@@ -22,11 +27,17 @@
   <script setup>
   import DemLayout from '@/Layouts/DemLayout.vue';
   import Drawer from '@/Components/Organisms/Drawer.vue';
+  import CreateClientForm from '@/Components/Organisms/CreateClientForm.vue';
   import DrawerContent from '@/Components/Molecules/DrawerContent.vue';
   import { Head } from '@inertiajs/vue3';
-  import { ref } from 'vue';
+  import { onMounted, ref } from 'vue';
 
   let isDrawerOpen = ref(false);
+  let innerWidth = ref(window.innerWidth/2 + 'px');
+
+  onMounted(() => {
+    console.log(innerWidth.value);
+  })
 
   const toggleDrawer = () => {
     isDrawerOpen.value = !isDrawerOpen.value;
