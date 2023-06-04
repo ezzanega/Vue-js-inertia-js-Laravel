48390c26537abf8ade4329eab3beab4d25fdd0b0
Meijink <junada99@gmail.com>
2023-06-05 01:17:44 +0200

    Editing routes for ACL Controller

--- routes/web.php
+++ routes/web.php
@@ -34,54 +34,40 @@ Route::get('/resetpassword', function () {
     return inertia('ResetPassword');
 })->name('6dem.reset-password');
 
+Route::get('/', function () {
+    return inertia('6dem/Index');
+});
 
 Route::get('/design-system', function () {
     return inertia('6dem/DesignSystem');
 });
 
+Route::get('/6dem/dashboard', function () {
+    return inertia('6dem/Dashboard');
+})->name('6dem.dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return inertia('6dem/Index');
-    });
+Route::get('/6dem/clients', function () {
+    return inertia('6dem/Clients');
+})->name('6dem.clients');
 
-<<<<<<< HEAD
 Route::get('/6dem/documents',function () {
     return inertia('6dem/Documents');
 } )->name('6dem.documents');
 
 Route::get('/6dem/manage', [AclController::class, 'create'])
     ->name('6dem.manage');
-=======
-    Route::get('/6dem/dashboard', function () {
-        return inertia('6dem/Dashboard');
-    })->name('6dem.dashboard');
 
-    Route::get('/6dem/clients', function () {
-        return inertia('6dem/Clients');
-    })->name('6dem.clients');
->>>>>>> 7414dbbd187b3b0370fe0d421a6ec892a8261629
+Route::get('/6dem/settings', function () {
+    return inertia('6dem/Settings');
+})->name('6dem.settings');
 
-    Route::get('/6dem/documents', function () {
-        return inertia('6dem/Documents');
-    })->name('6dem.documents');
+Route::get('/6dem/storage', function () {
+    return inertia('6dem/Storage');
+})->name('6dem.storage');
 
-    Route::get('/6dem/manage', function () {
-        return inertia('6dem/Manage');
-    })->name('6dem.manage');
-
-    Route::get('/6dem/settings', function () {
-        return inertia('6dem/Settings');
-    })->name('6dem.settings');
-
-    Route::get('/6dem/storage', function () {
-        return inertia('6dem/Storage');
-    })->name('6dem.storage');
-
-    Route::get('/6dem/templetes', function () {
-        return inertia('6dem/Templetes');
-    })->name('6dem.templetes');
-});
+Route::get('/6dem/templetes', function () {
+    return inertia('6dem/Templetes');
+})->name('6dem.templetes');
 
 
 
