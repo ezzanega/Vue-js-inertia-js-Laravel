13ffffd0a0a1b639d8308e2de1f4b6127e97bba0
Abdoul Zakari <60135885+abdoul-nft@users.noreply.github.com>
2023-06-23 19:03:24 +0200

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "Installing Node.js version $NODE_VERSION"
+curl -sL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+echo "Node.js version $NODE_VERSION installed successfully"
