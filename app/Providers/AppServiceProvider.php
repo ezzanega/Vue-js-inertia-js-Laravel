e53abf013794ba0b91089c14279f8bb5cf138f8b
Meijink <junada99@gmail.com>
2023-07-06 17:27:31 +0200

    Adding Observers into App Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,10 @@ use App\Models\User;
 use App\Observers\UserObserver;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use App\Observers\EmailTemplatesObserver;
+use App\Models\Quotation;
+use App\Observers\QuotationObserver;
+use App\Models\MovingJob;
+use App\Observers\MovingJobObserver;
 use App\Models\EmailTemplates;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(125);
         User::observe(UserObserver::class);
+        Quotation::observe(QuotationObserver::class);
+        MovingJob::observe(MovingJobObserver::class);
         EmailTemplates::observe(EmailTemplatesObserver::class);
     }
 }
