583aaec1976a97d3e44ec0988388a7cbcc808e76
ezzanega <ezzanegabouchra@gmail.com>
2023-08-02 14:28:35 +0100

    troi point de drop down list de la page mail Mail

--- resources/js/Components/Templates/MailTemplatesList.vue
+++ resources/js/Components/Templates/MailTemplatesList.vue
@@ -42,6 +42,7 @@
         </tr>
       </thead>
       <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+
         <tr
           v-for="(mail, index) in templates"
           :key="index"
@@ -82,7 +83,38 @@
               {{ mail.body }}
             </div>
           </td>
-          <td class="px-6 py-4">
+          <td>
+            <div class="truncate">
+              <div class="flex justify-end gap-4"></div>
+
+              <Dropdown placement="bottom-end">
+                <svg
+                  class="h-6 w-6 cursor-pointer"
+                  viewBox="0 0 28 7"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.9449 6.06482C15.6474 6.06482 17.0276 4.70716 17.0276 3.03241C17.0276 1.35766 15.6474 0 13.9449 0C12.2424 0 10.8623 1.35766 10.8623 3.03241C10.8623 4.70716 12.2424 6.06482 13.9449 6.06482Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M24.8082 6.06482C26.5107 6.06482 27.8909 4.70716 27.8909 3.03241C27.8909 1.35766 26.5107 0 24.8082 0C23.1057 0 21.7256 1.35766 21.7256 3.03241C21.7256 4.70716 23.1057 6.06482 24.8082 6.06482Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M3.08263 6.06482C4.78513 6.06482 6.16527 4.70716 6.16527 3.03241C6.16527 1.35766 4.78513 0 3.08263 0C1.38014 0 0 1.35766 0 3.03241C0 4.70716 1.38014 6.06482 3.08263 6.06482Z"
+                    fill="currentColor"
+                  />
+                </svg>
+                <template #popper>
+                  <MailActionsPopperContent />
+                </template>
+              </Dropdown>
+            </div>
+          </td>
+          <!-- <td class="px-6 py-4">
+            Bonjour
             <div class="truncate">
               <div class="flex justify-end gap-4"></div>
               <svg
@@ -101,14 +133,16 @@
                 />
               </svg>
             </div>
-          </td>
+          </td> -->
         </tr>
       </tbody>
     </table>
   </div>
 </template>
-  
-  <script setup>
+
+<script setup>
+import { Dropdown } from "floating-vue";
+import MailActionsPopperContent from "@/Components/Molecules/MailActionsPopperContent.vue";
 import CreateMailTemplateModal from "@/Components/Templates/CreateMailTemplateModal.vue";
 import IconButton from "@/Components/Atoms/IconButton.vue";
 import { usePage } from "@inertiajs/vue3";
