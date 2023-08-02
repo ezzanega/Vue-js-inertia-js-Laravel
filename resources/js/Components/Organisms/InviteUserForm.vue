10614631e9a1b675d2b147c5f816aee2cf84875d
ezzanega <ezzanegabouchra@gmail.com>
2023-08-02 14:42:00 +0100

    drop down list Dans la page de collaborateurs

--- resources/js/Components/Organisms/InviteUserForm.vue
+++ resources/js/Components/Organisms/InviteUserForm.vue
@@ -46,6 +46,7 @@
       <div class="px-4 sm:px-6">
         <div class="w-full py-3 flex flex-col space-y-2">
           <div class="my-6 space-y-5">
+            
             <DefaultInput
               :required="true"
               type="email"
@@ -77,7 +78,7 @@
     </div>
   </Modal>
 </template>
-  
+
   <script setup>
 import Modal from "@/Components/Modal.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
@@ -122,4 +123,4 @@ const closeModal = () => {
   form.reset();
   emit("closeModal");
 };
-</script>   
+</script>
--- resources/js/Components/Organisms/TeamMembersList.vue
+++ resources/js/Components/Organisms/TeamMembersList.vue
@@ -93,21 +93,30 @@
           </td>
           <td class="px-6 py-4">
             <div class="flex justify-end gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="h-6 w-6"
-                x-tooltip="tooltip"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
+                <Dropdown placement="bottom-end">
+                    <svg
+                      class="h-6 w-6 cursor-pointer"
+                      viewBox="0 0 28 7"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M13.9449 6.06482C15.6474 6.06482 17.0276 4.70716 17.0276 3.03241C17.0276 1.35766 15.6474 0 13.9449 0C12.2424 0 10.8623 1.35766 10.8623 3.03241C10.8623 4.70716 12.2424 6.06482 13.9449 6.06482Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M24.8082 6.06482C26.5107 6.06482 27.8909 4.70716 27.8909 3.03241C27.8909 1.35766 26.5107 0 24.8082 0C23.1057 0 21.7256 1.35766 21.7256 3.03241C21.7256 4.70716 23.1057 6.06482 24.8082 6.06482Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M3.08263 6.06482C4.78513 6.06482 6.16527 4.70716 6.16527 3.03241C6.16527 1.35766 4.78513 0 3.08263 0C1.38014 0 0 1.35766 0 3.03241C0 4.70716 1.38014 6.06482 3.08263 6.06482Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                    <template #popper>
+                      <ManageCollaborateurActionsPopperContent />
+                    </template>
+                </Dropdown>
             </div>
           </td>
         </tr>
@@ -117,25 +126,27 @@
 </template>
 
 <script setup>
-import IconButton from "@/Components/Atoms/IconButton.vue";
-import InviteUserForm from "@/Components/Organisms/InviteUserForm.vue";
-import { usePage } from "@inertiajs/vue3";
-import { ref } from "vue";
-import { getRoleLabel } from "@/utils/index";
+    import { Dropdown } from "floating-vue";
+    import ManageCollaborateurActionsPopperContent from "@/Components/Molecules/ManageCollaborateurActionsPopperContent.vue";
+    import IconButton from "@/Components/Atoms/IconButton.vue";
+    import InviteUserForm from "@/Components/Organisms/InviteUserForm.vue";
+    import { usePage } from "@inertiajs/vue3";
+    import { ref } from "vue";
+    import { getRoleLabel } from "@/utils/index";
 
-defineProps({
-  teamMembres: Array,
-});
+    defineProps({
+    teamMembres: Array,
+    });
 
-const roles = usePage().props.roles;
+    const roles = usePage().props.roles;
 
-const inviteUserModal = ref(false);
+    const inviteUserModal = ref(false);
 
-const openInviteUserModal = () => {
-  inviteUserModal.value = true;
-};
+    const openInviteUserModal = () => {
+    inviteUserModal.value = true;
+    };
 
-const closeModal = () => {
-  inviteUserModal.value = false;
-};
+    const closeModal = () => {
+    inviteUserModal.value = false;
+    };
 </script>
