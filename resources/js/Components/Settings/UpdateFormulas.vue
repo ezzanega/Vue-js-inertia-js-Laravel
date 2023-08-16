fcec8d90b92498c6694245ca918567b8b7fa7e9b
ezzanega <ezzanegabouchra@gmail.com>
2023-08-16 16:33:43 +0100

    Adding Add Formulas Button

--- resources/js/Components/Settings/UpdateFormulas.vue
+++ resources/js/Components/Settings/UpdateFormulas.vue
@@ -35,16 +35,24 @@
                     </p>
                 </div>
             </div>
-            <h5 v-else class=" flex mb-3 text-base font-semibold text-gray-900 md:text-xl">
-              {{ formula.name }}
-              <button @click="toggleInputFormulasField(index)">
+            <h5 v-else class="flex justify-between items-center mb-3 text-base font-semibold text-gray-900 md:text-xl">
+                <div>
+                  {{ formula.name }}
+                  <button @click="toggleInputFormulasField(index)">
                     <svg class="h-4 w-5 ml-1 text-gray-300 hover:text-green-700 transition-colors duration-200"
-                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
+                      fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                     </svg>
-              </button>
+                  </button>
+                </div>
+                <button>
+                  <svg class="h-6 w-6 text-black hover:text-red-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                  </svg>
+                </button>
+              </h5>
 
-            </h5>
             <div class="space-x-2 xl:flex">
               <div class="w-auto xl:w-1/2">
                 <p class="text-sm font-normal text-gray-500">À notre charge</p>
