44425b6fc1a539a7fd632873ada74a7b30b85db4
Meijink <junada99@gmail.com>
2023-08-18 10:00:24 +0200

    Fixing All menu titles selected on sidebar when route is /

--- resources/js/Components/Atoms/SideBarItem.vue
+++ resources/js/Components/Atoms/SideBarItem.vue
@@ -1,7 +1,12 @@
 <template>
   <Link :href="route(itemroute)"
     class="hover:bg-secondary hover:text-primary group relative flex items-center rounded-md px-2 py-3 text-md font-bold"
-    :class="[(route(itemroute).includes($page.url.split('/').slice(1, 3).join('/') || '')) ? 'text-primary bg-secondary' : 'text-white']"
+    :class="[
+      (($page.url !== '/' && route(itemroute).includes($page.url.split('/').slice(1, 3).join('/')))
+        || ($page.url === '/' && route(itemroute).includes('/6dem/dashboard')))
+        ? 'text-primary bg-secondary'
+        : 'text-white'
+    ]"
   >
     <slot />
     {{ item }}
