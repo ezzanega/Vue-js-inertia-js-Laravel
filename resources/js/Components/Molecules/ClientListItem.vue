aa2a5be953c643778da651fa07792f380da71607
ezzanega <ezzanegabouchra@gmail.com>
2023-08-21 12:41:08 +0100

    Update Icon Remove create option

--- resources/js/Components/Molecules/ClientListItem.vue
+++ resources/js/Components/Molecules/ClientListItem.vue
@@ -29,7 +29,7 @@
             v-if="client.type == 'professional'"
             class="inline-flex items-center gap-1 rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-600"
           >
-            Professionnel
+            Professionnel   
           </span>
           <span
             v-else
