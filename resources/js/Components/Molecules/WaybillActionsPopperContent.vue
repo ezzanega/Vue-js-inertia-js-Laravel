6141d8797423422b60e6e9aeeede26b076f3bafa
ezzanega <ezzanegabouchra@gmail.com>
2023-09-12 12:29:09 +0100

    Envoyer Facture - devis -Lv au client

--- resources/js/Components/Molecules/WaybillActionsPopperContent.vue
+++ resources/js/Components/Molecules/WaybillActionsPopperContent.vue
@@ -23,7 +23,7 @@
             d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
         </svg>
       </PopperItem> -->
-      <PopperItem item="Envoyer au client" @clicked="PopperItemClicked">
+      <PopperItem item="Envoyer au client" @clicked="sendToClient">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
           class="mr-2 h-5 w-5 shrink-0 text-neutral-500 group-hover:text-neutral-600">
           <path stroke-linecap="round" stroke-linejoin="round"
@@ -76,4 +76,11 @@ const updateQuotation = () => {
     method: "get",
   });
 };
+
+const sendToClient = () => {
+  router.visit(route("6dem.documents.send.waybill", [props.document.id]), {
+    method: "get",
+  });
+};
+
 </script>
