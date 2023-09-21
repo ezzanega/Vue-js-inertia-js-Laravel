b7e39ea48f0c6fa9cc581e600e0db627cc93e24b
ezzanega <ezzanegabouchra@gmail.com>
2023-09-21 15:14:15 +0100

    add update icon calendar

--- resources/js/Components/Calendar/EventDetails.vue
+++ resources/js/Components/Calendar/EventDetails.vue
@@ -35,7 +35,7 @@
                         <div class="flex flex-col space-y-1 bg-secondary rounded-md p-2 text-sm">
                             <div class="flex justify-between">
                                 <span class="flex font-bold">Détails:</span>
-                                <svg class="h-5 w-5 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg class="h-5 w-5 text-black cursor-pointer"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                                 </svg>
                             </div>
